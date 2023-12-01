459c66dbe1acc484e9299b041f347b3b001dd57a
quy dat truong <dattq36@fpt.com>
2023-12-01 10:22:39 +0700

    commit env

--- datasource.config.ts
+++ datasource.config.ts
@@ -7,7 +7,7 @@ export const AppDataSource = new DataSource({
     host: "127.0.0.1",
     port: 3306,
     username: "root",
-    password: "Langngo@98",
+    password: "",
     database: "test",
     entities: [Book],
     synchronize: true,
